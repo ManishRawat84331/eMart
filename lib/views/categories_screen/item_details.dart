006daf8700ec5a80ad6d2207d5d0880c50866f09
manishrawat0607 <manishrawat0607@gmail.com>
2024-01-09 21:40:22 +0530

    Added items detials screen

--- lib/views/categories_screen/item_details.dart
+++ lib/views/categories_screen/item_details.dart
@@ -1,5 +1,6 @@
 import 'package:e_commerce/consts/colors.dart';
 import 'package:e_commerce/consts/consts.dart';
+import 'package:e_commerce/widgets/button.dart';
 import 'package:flutter/material.dart';
 
 class ItemDetails extends StatelessWidget {
@@ -17,6 +18,79 @@ class ItemDetails extends StatelessWidget {
             IconButton(onPressed: () {}, icon: Icon(Icons.favorite_outline)),
           ],
         ),
-        body: Container());
+        body: Column(
+          children: [
+            Expanded(
+                child: Padding(
+                    padding: EdgeInsets.all(8),
+                    child: SingleChildScrollView(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          // swiper section
+                          VxSwiper.builder(
+                              autoPlay: true,
+                              aspectRatio: 16 / 9,
+                              height: 350,
+                              itemCount: 3,
+                              itemBuilder: ((context, index) {
+                                return Image.asset(
+                                  imgFc5,
+                                  width: double.infinity,
+                                  fit: BoxFit.cover,
+                                );
+                              })),
+
+                          10.heightBox,
+                          title!.text
+                              .size(16)
+                              .color(darkFontGrey)
+                              .fontFamily(bold)
+                              .make(),
+                          10.heightBox,
+                          // rating bar
+                          VxRating(
+                            onRatingUpdate: (value) {},
+                            normalColor: textfieldGrey,
+                            selectionColor: golden,
+                            count: 5,
+                            size: 25,
+                            stepInt: true,
+                          ),
+                          10.heightBox,
+                          "\$3000"
+                              .text
+                              .color(redColor)
+                              .fontFamily(bold)
+                              .size(18)
+                              .make(),
+                          10.heightBox,
+                          Row(
+                            children: [
+                              Expanded(
+                                child: Column(),
+                              ),
+                              CircleAvatar(
+                                backgroundColor: Colors.white,
+                                child: Icon(
+                                  Icons.message_rounded,
+                                  color: darkFontGrey,
+                                ),
+                              )
+                            ],
+                          )
+                        ],
+                      ),
+                    ))),
+            SizedBox(
+              width: double.infinity,
+              child: Button(
+                  color: redColor,
+                  onPress: () {},
+                  textColor: whiteColor,
+                  title: "Add To Cart"),
+            )
+          ],
+        ));
   }
 }
